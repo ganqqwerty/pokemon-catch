013770891ddde377f38a61dd196475e0c86e3061
Iurii Katkov <iurii.katkov@mimacom.com>
2020-08-03 03:03:10 +0200

    Updates

---
+++ precache-manifest.f6038ef91d24317821b33103d5956937.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6039e4c0607ebe2fd23e056ae9146b50",
+    "url": "/pokemon-catch/index.html"
+  },
+  {
+    "revision": "d3a97a6c266fdf06dd4b",
+    "url": "/pokemon-catch/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "271083fa4c91e7dbf9c4",
+    "url": "/pokemon-catch/static/css/main.d6bd3934.chunk.css"
+  },
+  {
+    "revision": "d3a97a6c266fdf06dd4b",
+    "url": "/pokemon-catch/static/js/2.e7164a8c.chunk.js"
+  },
+  {
+    "revision": "271083fa4c91e7dbf9c4",
+    "url": "/pokemon-catch/static/js/main.eae920b5.chunk.js"
+  },
+  {
+    "revision": "298f00ab2d1bcac7ecf8",
+    "url": "/pokemon-catch/static/js/runtime~main.221150fe.js"
+  },
+  {
+    "revision": "2f43e93aa559ebd97080d078810818c9",
+    "url": "/pokemon-catch/static/media/yuri-pokemon.2f43e93a.png"
+  }
+]);
